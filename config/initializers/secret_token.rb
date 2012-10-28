4ca58748bd655e8ab53600f9de24bb927e6d9385
Tum Chaturapruek <Tum_Chaturapruek@HMC.Edu>
2012-10-27 17:33:19 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OLP::Application.config.secret_token = '95cd0bd4a6feed2164ff54f9dac65c61c961aebcdecaf18ec80139a12764f826ba4520090d505b23011684677c17ada4d9972b12f9da9c1828c0a6f6914dc0bc'
