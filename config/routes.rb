4cb31e3b9ac73fd1b9786b747203b17fc142de5f
Tum Chaturapruek <tummykung@gmail.com>
2012-12-30 04:36:22 -0800

    add more to the projects page

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ OLP::Application.routes.draw do
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
   match '/signup',  to: 'users#new'
+  match '/learnstream', to: 'static_pages#learnstream'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
