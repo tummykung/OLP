3acf21eb068b01b76f94f63e16c0b2b1d345c8f7
Tum Chaturapruek <tummykung@gmail.com>
2013-01-03 01:41:57 -0800

    finish signup/signin/signout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 OLP::Application.routes.draw do
-  get "users/new"
+  
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
 
   root to: 'static_pages#home'
   match '/help',    to: 'static_pages#help'
@@ -10,9 +13,12 @@ OLP::Application.routes.draw do
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
   match '/secret',  to: 'static_pages#secret'
-  match '/signup',  to: 'users#new'
   match '/learnstream', to: 'static_pages#learnstream'
-
+  
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new' 
+  match '/signout',  to: 'sessions#destroy'
+    
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
