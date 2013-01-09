4b0fcc73daf0456da3ca845556bbcaae4e70d746
Tum Chaturapruek <tummykung@gmail.com>
2013-01-09 11:17:29 -0800

    try again

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => ENV['muddsd-olp'],
-  :password       => ENV['a1s2d3f4g5'],
+  :user_name      => ENV['app8782010@heroku.com'],
+  :password       => ENV['gey1ivwh'],
   :domain         => 'heroku.com'
 }
 ActionMailer::Base.delivery_method = :smtp
