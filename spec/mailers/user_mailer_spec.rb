3060c812dd87c4cd3c1be1d3f82aad58cc560ba9
Tum Chaturapruek <tummykung@gmail.com>
2013-01-09 09:13:28 -0800

    halfway through emails

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,12 @@
-require "spec_helper"
+require "email_spec"
 
 describe UserMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+	describe "POST /signup (#signup)" do
+		it "should deliver the signup email" do
+	    # expect
+	    UserMailer.should_receive(:deliver_signup).with("email@example.com", "Jimmy Bean")
+	    # when
+	    post :signup, "Email" => "email@example.com", "Name" => "Jimmy Bean"
+	    end
+	end
 end
